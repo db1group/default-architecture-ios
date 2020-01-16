303a7f759c2e61f32ea682ed7c673e33a255c634
Glauco <dantas.glauco@gmail.com>
2020-01-16 14:05:15 -0300

    Framework project created

---
+++ KeychainService/KeychainService/KeychainService.h
@@ -0,0 +1,19 @@
+//
+//  KeychainService.h
+//  KeychainService
+//
+//  Created by Glauco Dantas Beserra  on 16/01/20.
+//  Copyright © 2020 Glauco Dantas Beserra . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KeychainService.
+FOUNDATION_EXPORT double KeychainServiceVersionNumber;
+
+//! Project version string for KeychainService.
+FOUNDATION_EXPORT const unsigned char KeychainServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeychainService/PublicHeader.h>
+
+
