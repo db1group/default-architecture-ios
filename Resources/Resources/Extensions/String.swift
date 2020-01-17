6cc3db044999ca7e30ef5ccdfeac7b1e87d54912
Elison José Gracite Lusvardi <elison.lusvardi@db1.com.br>
2020-01-17 15:37:27 -0300

    add new extensions

--- Resources/Resources/Extensions/String.swift
+++ Resources/Resources/Extensions/String.swift
@@ -49,12 +49,123 @@ extension String {
         return temp1 == d1 && temp2 == d2
     }
 
+    // MARK: - Validação de CNPJ
+    var isCNPJ: Bool {
+        if self.count != 14 {
+            return false
+        }
+
+        if self == "00000000000000" {
+            return false
+        }
+
+        if self == "11111111111111" {
+            return false
+        }
+
+        if self == "22222222222222" {
+            return false
+        }
+
+        if self == "33333333333333" {
+            return false
+        }
+
+        if self == "44444444444444" {
+            return false
+        }
+
+        if self == "55555555555555" {
+            return false
+        }
+
+        if self == "66666666666666" {
+            return false
+        }
+
+        if self == "77777777777777" {
+            return false
+        }
+
+        if self == "88888888888888" {
+            return false
+        }
+
+        if self == "99999999999999" {
+            return false
+        }
+
+        var input: String = ""
+        input = self
+
+        let cnpj = input.filter{"0123456789".contains($0)}
+
+
+        var isValidCNPJ: Bool {
+            let numbers = cnpj.compactMap({Int(String($0))})
+            guard numbers.count == 14 && Set(numbers).count != 1 else { return false }
+            let soma1 = 11 - ( numbers[11] * 2 +
+                numbers[10] * 3 +
+                numbers[9] * 4 +
+                numbers[8] * 5 +
+                numbers[7] * 6 +
+                numbers[6] * 7 +
+                numbers[5] * 8 +
+                numbers[4] * 9 +
+                numbers[3] * 2 +
+                numbers[2] * 3 +
+                numbers[1] * 4 +
+                numbers[0] * 5 ) % 11
+            let dv1 = soma1 > 9 ? 0 : soma1
+            let soma2 = 11 - ( numbers[12] * 2 +
+                numbers[11] * 3 +
+                numbers[10] * 4 +
+                numbers[9] * 5 +
+                numbers[8] * 6 +
+                numbers[7] * 7 +
+                numbers[6] * 8 +
+                numbers[5] * 9 +
+                numbers[4] * 2 +
+                numbers[3] * 3 +
+                numbers[2] * 4 +
+                numbers[1] * 5 +
+                numbers[0] * 6 ) % 11
+            let dv2 = soma2 > 9 ? 0 : soma2
+            return dv1 == numbers[12] && dv2 == numbers[13]
+        }
+
+        return isValidCNPJ
+
+    }
+
+    var isEmail: Bool {
+         if self.isEmpty {
+             return false
+         }
+         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+         let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+         return emailTest.evaluate(with: self)
+     }
+
+     func formatCurrency(value: Double) -> String {
+         let formatter = NumberFormatter()
+         formatter.numberStyle = .currency
+         formatter.locale = Locale(identifier: "pt_BR")
+         return formatter.string(from: NSNumber(value: value)) ?? "R$0,00"
+     }
+
     func fromBase64() -> String? {
         guard let data = Data(base64Encoded: self) else {
             return nil
         }
 
         return String(data: data, encoding: .utf8)
+    }
+
+    func toDate(formato: String) -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = formato
+        return dateFormatter.date(from: self)!
     }
 
     func toBase64() -> String {
---
+++ Resources/Resources/Extensions/UIView.swift
@@ -0,0 +1,86 @@
+//
+//  UIView.swift
+//  Resources
+//
+//  Created by Elison José Gracite Lusvardi on 17/01/20.
+//  Copyright © 2020 DB1. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+extension UIView {
+
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            guard let cgColor = layer.borderColor else {
+                return nil
+            }
+            return UIColor(cgColor: cgColor)
+        }
+
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+
+        set {
+            layer.borderWidth = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            guard let cgColor = layer.shadowColor else {
+                return nil
+            }
+            return UIColor(cgColor: cgColor)
+        }
+
+        set {
+            layer.shadowColor = shadowColor?.cgColor
+            layer.shadowOpacity = 0.4
+            layer.shadowOffset = CGSize.zero
+            layer.shadowRadius = 5
+            layer.masksToBounds = false
+        }
+    }
+
+    func addDashedBorder() {
+
+        let shapeLayer = CAShapeLayer()
+        let frameSize = frame.size
+        let shapeRect = CGRect(x: 0, y: 0, width: frameSize.width, height: frameSize.height)
+
+        shapeLayer.bounds = shapeRect
+        shapeLayer.position = CGPoint(x: frameSize.width/2, y: frameSize.height/2)
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = borderColor?.cgColor ?? UIColor.darkGray.cgColor
+        shapeLayer.lineWidth = 1
+        shapeLayer.lineJoin = CAShapeLayerLineJoin.round
+        shapeLayer.lineDashPattern = [6, 3]
+        shapeLayer.path = UIBezierPath(roundedRect: shapeRect, cornerRadius: cornerRadius).cgPath
+        shapeLayer.masksToBounds = true
+        layer.masksToBounds = true
+        layer.addSublayer(shapeLayer)
+    }
+
+}
+
